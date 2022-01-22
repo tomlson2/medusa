abdf1ad087d6d05d5091065660020993ef65e667
tomlson2 <thomasmbryson@gmail.com>
2022-01-22 14:55:01 -0700

    fixed time bug

--- windowcapture.py
+++ windowcapture.py
@@ -224,15 +224,15 @@ class Interactions(WindowCapture, Vision):
     def amount(self, item, threshold):
         return len(item.find(self.apply_hsv_filter(self.get_screenshot(),hsv_filter=item.get_hsv_filter()),threshold))
 
-    def wait_for(self, item : object, threshold : float = 0.7, time: int = 5):
+    def wait_for(self, item : object, threshold : float = 0.7, t: int = 5):
         """
         Waits for something to appear on screen region with a 10 second timeout period.
         """
         start_time = time.time()
         while self.contains(item, threshold) == False:
             current_time = (time.time() - start_time)
-            if current_time > time:
-                print(f"Failed to find Needle in {time} seconds")
+            if current_time > t:
+                print(f"Failed to find Needle in {t} seconds")
                 break
 class CustomRegion(Interactions):
 
